d28250582d02ffeed3cb3e1003527177b54e5e1d
sam85avila <sam85avila@gmail.com>
2023-02-08 08:56:42 -0300

    Primeiro Projeto

---
+++ PrimeiroProjeto/src/br/com/ebac/PrimeiroProjeto.java
@@ -0,0 +1,13 @@
+package br.com.ebac;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		 
+		String nome = "EBAC";		 
+		 
+		System.out.println("Olá, " + nome + ". Tudo bem?");			 
+		
+	}
+
+}
