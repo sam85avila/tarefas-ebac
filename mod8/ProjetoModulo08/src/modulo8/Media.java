77f614fea2fde0a1d9630faaea28711b15dd4e13
sam85avila <sam85avila@gmail.com>
2023-02-09 14:32:14 -0300

    Projeto do Módulo 8

---
+++ mod8/ProjetoModulo08/src/modulo8/Media.java
@@ -0,0 +1,18 @@
+package modulo8;
+
+public class Media {
+
+    public static void main(String[] args) {
+
+        double nota1 = 7.25;
+        double nota2 = 6.5;
+        double nota3 = 9.75;
+        double nota4 = 10;
+        double media;
+
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("A média final é igual a " + media);
+
+    }
+}
