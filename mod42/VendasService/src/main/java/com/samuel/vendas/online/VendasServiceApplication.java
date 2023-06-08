4fe021267a21e7d2d282e8fc36a9c3d2439c449f
samuel-ferreira-85 <sam85avila@gmail.com>
2023-06-08 16:02:25 -0300

    Finalização do Microsserviço de Vendas

--- mod42/VendasService/src/main/java/com/samuel/vendas/online/VendasServiceApplication.java
+++ mod42/VendasService/src/main/java/com/samuel/vendas/online/VendasServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RefreshScope
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
+@RefreshScope
+@EnableFeignClients
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class VendasServiceApplication {
 
 	public static void main(String[] args) {
